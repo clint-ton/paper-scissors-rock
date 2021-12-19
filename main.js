5497307d0a3e73dba63c2f4d4922fa3f154891a9
clinton <cjw237@uclive.ac.nz>
2021-12-19 14:52:44 +1300

    Added user interface

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const moves = ['paper', 'scissors', 'rock'];
+const playerScoreSquares = [null, 'p1', 'p2', 'p3']
+const computerScoreSquares = [null, 'c1', 'c2', 'c3']
+let playerScore = 0;
+let computerScore = 0;
 
+const refreshBtn = document.createElement('button');
+refreshBtn.textContent = 'Play Again';
+refreshBtn.classList.add('refresh')
+
+const body = document.querySelector('body');
+const resultEle = document.querySelector('.results');
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -7,6 +17,17 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function paperPlay() {
+    updateGame(playRound(computerPlay(), 'paper'));
+}
+
+function scissorsPlay() {
+    updateGame(playRound(computerPlay(), 'scissors'));
+}
+
+function rockPlay() {
+    updateGame(playRound(computerPlay(), 'rock'));
+}
 function computerPlay() {
     return moves[getRandomInt(0, 3)];
 }
@@ -15,62 +36,86 @@ function playRound (computerMove, playerMove){
     if (computerMove == 'paper') {
         switch (playerMove) {
             case 'paper':
-                result = 'draw';
+                result = 'Draw.';
                 break;
             case 'scissors':
-                result = 'player';
+                result = 'Win!';
                 break;
             case 'rock':
-                result = 'computer';
+                result = 'Lose.';
                 break;
         } 
     } else if (computerMove == 'scissors') {
         switch (playerMove) {
             case 'paper':
-                result = 'computer';
+                result = 'Lose.';
                 break;
             case 'scissors':
-                result = 'draw';
+                result = 'Draw.';
                 break;
             case 'rock':
-                result = 'player';
+                result = 'Win!';
                 break;
         }
     } else if (computerMove = 'rock') {
         switch (playerMove) {
             case 'paper':
-                result = 'player';
+                result = 'Win!';
                 break;
             case 'scissors':
-                result = 'computer';
+                result = 'Lose.';
                 break;
             case 'rock':
-                result = 'draw';
+                result = 'Draw.';
                 break;
         }
     }
     return result;
 }
 
-function game () {
-    playerScore = 0;
-    computerScore = 0;
+function updateGame(result) {
+    const h1 = document.querySelector('h1');
+    h1.textContent = result;
 
-
-    while (playerScore < 3 && computerScore < 3) {
-        let playerMove = prompt('What move would you like to make?')
-        let result = playRound(computerPlay(), playerMove);
-        if (result == 'player') {
-            playerScore++;
-        } else if (result == 'computer') {
-            computerScore++;
-        }
-        let scoreString = `Player :${playerScore} Computer ${computerScore}`;
-        console.log(scoreString);
-
+    if (result == 'Win!') {
+        playerScore++;
+        let id = playerScoreSquares[playerScore];
+        const square = document.querySelector(`.${id}`);
+        square.classList.add('ticked-box');
+    } else if (result == 'Lose.') {
+        computerScore++;
+        let id = computerScoreSquares[computerScore];
+        const square = document.querySelector(`.${id}`);
+        square.classList.add('crossed-box');
     }
 
+    if (playerScore > 2) {
+        h1.textContent = 'Game Over\n You win!';
+        body.insertBefore(refreshBtn, resultEle);
+        unbindButtons();
 
+    } else if ( computerScore > 2) {
+        h1.textContent = 'Game Over\n You lose.'
+        body.insertBefore(refreshBtn, resultEle);
+        unbindButtons();
+
+    }
 }
 
-game();
+function unbindButtons() {
+    paper.removeEventListener('click', paperPlay);
+    scissors.removeEventListener('click', scissorsPlay);
+    rock.removeEventListener('click', rockPlay);
+}
+const paper = document.getElementById('paper');
+paper.addEventListener ('click', paperPlay);
+
+const scissors = document.getElementById('scissors');
+scissors.addEventListener ('click', scissorsPlay);
+
+const rock = document.getElementById('rock');
+rock.addEventListener ('click', rockPlay);
+
+refreshBtn.addEventListener ('click', () => {
+    location.reload();
+})
